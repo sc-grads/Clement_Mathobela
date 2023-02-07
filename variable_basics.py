976ac3c576741e36f13d6f1e9c6a1c556060d26d
Clement Mathobela <clement.mathobela@sambeconsulting.com>
2023-02-07 22:52:43 +0200

    python variables with difference data typessss....

---
+++ variable_basics.py
@@ -0,0 +1,11 @@
+# define a variable named product_name and set it to any string
+product_name = 'Bicycle'
+
+# define a variable named quantity and set it to any integer
+quantity = 2
+
+# define a variable called price and set it to any float
+price = 3500.99
+
+# define variable called sold and set it to any boolean value
+sold = True
