831142a50c47a0bdce034b384020a156e0193e88
clement.mathobela@sambeconsulting.com <clement.mathobela@sambeconsulting.com>
2023-02-08 11:54:46 +0200

    Solution for challenge 2

---
+++ PythonBasics/challenge_2.py
@@ -0,0 +1,13 @@
+# Change this script so that it follows the Python naming and style conventions described in PEP8.
+
+my_name = 'Andrei'
+value = 100
+
+
+# This is
+# an example of a multiline
+# comment in Python.
+
+
+text = 'Hello'
+print(text)
