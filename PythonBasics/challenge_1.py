8291abafa5529686eae3bf604c1538029ed14e27
clement.mathobela@sambeconsulting.com <clement.mathobela@sambeconsulting.com>
2023-02-08 11:45:25 +0200

    Added a new file for challenge #1

---
+++ PythonBasics/challenge_1.py
@@ -0,0 +1,12 @@
+
+age = 18
+weight = 72.5
+name = "Bob"
+interests = ['art', 'cooking', 'sports']
+male = True
+
+print(age, "is", type(age))
+print(weight, "is", type(weight))
+print(name, "is", type(name))
+print(interests,"is", type(interests))
+print(male,"is",type(male))
