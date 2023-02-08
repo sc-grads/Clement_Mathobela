75b46c0a702f15a98cdd267a80ff20af3f87f660
clement.mathobela@sambeconsulting.com <clement.mathobela@sambeconsulting.com>
2023-02-08 12:37:14 +0200

    Solution for python basics challenge 4

---
+++ PythonBasics/challenge_4.py
@@ -0,0 +1,15 @@
+a = 10  # a is 10
+a += 2  # a is now 12
+a *= 3  # a is now 36
+b = 4  # b is 4
+print(a == b)  # 36 does not equal 4
+print(a >= b)  # #6 is greater than 4
+c = a * b  # a multiply by b
+print(c)
+d = b ** 2   # b to the power 2
+print(d)
+print(c/2)
+e = a // 3  # floor division
+print(e)
+f = 20 % b    # 20 mod 4
+print(f)
