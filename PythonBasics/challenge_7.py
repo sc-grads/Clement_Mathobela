f245d2b8ac281ddc3de53f598977a1eb2e9c4945
clement.mathobela@sambeconsulting.com <clement.mathobela@sambeconsulting.com>
2023-02-08 13:19:23 +0200

    sollution for challenge 7

---
+++ PythonBasics/challenge_7.py
@@ -0,0 +1,8 @@
+#  A company's revenue is 45,897,513.
+#  Calculate the company's profit if the profit represents 12.7% of the revenue.
+#  Display the profit using 2 decimal places.
+
+company_revenue = 45_897_513
+company_profit = company_revenue * (12.7/100)
+print(round(company_profit, 2))
+
