e47e3b095872a34219125f28d53e3c2621b8b1b2
clement.mathobela@sambeconsulting.com <clement.mathobela@sambeconsulting.com>
2023-02-08 12:03:04 +0200

    Solution for challenge 3

---
+++ PythonBasics/challenge_3.py
@@ -0,0 +1,13 @@
+# This script contains some syntax errors.
+# Modify the script so that it runs without any errors.
+
+best_friend = 'Anne'
+print('My best friend is ', best_friend)
+
+age = 18
+print(age == 10)
+
+a, b = 10, 20
+print(a + b)
+
+print('To comment a line of code you # at the beginning of the line.')
