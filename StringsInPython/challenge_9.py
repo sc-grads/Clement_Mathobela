61f32e95450d58b87bc1e65f90d5ac9b12062c3c
clement.mathobela@sambeconsulting.com <clement.mathobela@sambeconsulting.com>
2023-02-09 15:15:26 +0200

    Solution to challenge 9

---
+++ StringsInPython/challenge_9.py
@@ -0,0 +1,6 @@
+# Write a Python script that removes the characters
+# which have odd index values of a given string.
+
+my_string = input("Enter a word/sentence:")
+new_string = my_string[::2]
+print(new_string)
