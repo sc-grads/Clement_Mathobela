23d0b34a292660873b79bcb14227a9d397b7958f
clement.mathobela@sambeconsulting.com <clement.mathobela@sambeconsulting.com>
2023-02-10 09:12:04 +0200

    Solution for exercise 10: f Elif and Else Statements

---
+++ Program Flow Control in Python/exercise_10.py
@@ -0,0 +1,18 @@
+# DON'T TOUCH THIS ###
+import random
+
+animals = ['cat', 'mouse', 'fish']
+choice = random.choice(animals)
+# DON'T TOUCH THIS ###
+
+
+# YOUR CODE STARTS HERE:
+
+if choice == 'cat':
+    print('Tom')
+
+elif choice == 'mouse':
+    print('Jerry')
+
+else:
+    print('Neo')
