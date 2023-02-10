6afdc297a5c16b14f416d0a57fa293f3a00de922
clement.mathobela@sambeconsulting.com <clement.mathobela@sambeconsulting.com>
2023-02-10 12:57:08 +0200

    Solution for exercise 12

---
+++ PythonLoops/exercise_12.py
@@ -0,0 +1,11 @@
+# YOUR CODE GOES HERE:
+
+my_sum = 0
+my_product = 1
+
+for value in range(1, 26):
+    my_sum += value
+    my_product *= value
+
+print(F'The sum is:{my_sum}')
+print(f'The product is: {my_product}')
