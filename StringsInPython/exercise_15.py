12f62c817e96fa3668b9d60bc7dfa0b29239dd16
clement.mathobela@sambeconsulting.com <clement.mathobela@sambeconsulting.com>
2023-02-11 22:47:29 +0200

    Deleted solutions for exercise 15 and project from StringsInPython

--- StringsInPython/exercise_15.py
+++
@@ -1,8 +0,0 @@
-names_list = ['Alice', 'Bob', 'Eve']
-# Converting list to string
-names_str = ','.join(names_list)
-
-
-url= 'www.python.org'
-# Converting string to list
-url_list = url.split('.')
