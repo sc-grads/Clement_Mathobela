117d8e20a82de6173cff71821fa4a799da589181
clement.mathobela@sambeconsulting.com <clement.mathobela@sambeconsulting.com>
2023-02-11 13:27:12 +0200

    Solution for challenge 3

--- PythonLoops/challenge_3.py
+++ PythonLoops/challenge_3.py
@@ -1,5 +1,6 @@
 # Write a Python program that counts and displays the vowels of a given string ignoring the letter case.
 my_str = input('Enter something:').lower()
+
 vowels = 'aeiou'
 count = 0
 for i in my_str:
