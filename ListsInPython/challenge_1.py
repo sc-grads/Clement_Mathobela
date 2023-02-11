5073ff660f4f5eb52aefac8c4767f4f1fa98bf2e
clement.mathobela@sambeconsulting.com <clement.mathobela@sambeconsulting.com>
2023-02-12 00:27:20 +0200

    Solution for challenge 1

---
+++ ListsInPython/challenge_1.py
@@ -0,0 +1,6 @@
+my_list = ['South', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Svalbard']
+print(my_list)
+country_remove = input('What should I remove from the list?:').capitalize()
+my_list.remove(country_remove)
+
+print(my_list)
