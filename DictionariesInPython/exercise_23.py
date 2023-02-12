c0576991a9fef25ceb3d838592938585aa0304fe
clement.mathobela@sambeconsulting.com <clement.mathobela@sambeconsulting.com>
2023-02-12 21:30:40 +0200

    Solution for coding exercise 23

---
+++ DictionariesInPython/exercise_23.py
@@ -0,0 +1,16 @@
+money = {
+    'bank': 8564.61,
+    'paypal': 1998.21,
+    'cash': 480,
+    'payoneer': 250.5
+}
+
+# YOUR CODE STARTS HERE
+
+# Use a for loop to iterate over the dictionary and calculate the total amount.
+# Save it to a variable called total_amount and print its value!
+total_amount = 0
+
+for i in money.values():
+    total_amount += i
+print(total_amount)
