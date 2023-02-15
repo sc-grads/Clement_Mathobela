9c74aa9c93ca4c461dcdc3ea7f95e669b0fdfe12
Clement Mathobela <clement.mathobela@sambeconsulting.com>
2023-02-15 12:02:49 +0200

    Solution for exercise 38

---
+++ MassiveSectionOfCodingChallengeExercises/exercise_38.py
@@ -0,0 +1,8 @@
+names1 = {'John', 'Marry', 'Lena', 'Pollux'}
+names2 = {'Dan', 'Arthur', 'Marry', 'Lena', 'Castor'}
+
+# Using set methods create a list called names that contains the elements that belong to both sets
+# names will be  ['Lena', 'Marry']
+
+# YOUR CODE STARTS HERE
+names = list(names1.intersection(names2))
