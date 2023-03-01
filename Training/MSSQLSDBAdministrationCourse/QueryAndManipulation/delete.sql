8527d35e5187dc3070a702555bbb8bcd87d5596c
Clement Mathobela <clement.mathobela@sambeconsulting.com>
2023-03-01 15:24:21 +0200

    Delete statement

---
+++ Training/MSSQLSDBAdministrationCourse/QueryAndManipulation/delete.sql
@@ -0,0 +1,24 @@
+
+USE AdventureWorks2019
+
+SELECT * FROM Sales.vIndividualCustomer
+
+CREATE TABLE Customer(
+CustomerID INT NOT NULL ,
+CustomerName VARCHAR(30) NOT NULL,
+PhoneNumber VARCHAR(30) NOT NULL,
+Email VARCHAR(50) NOT NULL
+)
+DROP TABLE Customer
+
+INSERT INTO Customer
+SELECT BusinessEntityID, FirstName,PhoneNumber,EmailAddress FROM Sales.vIndividualCustomer
+
+SELECT * FROM Customer
+
+Begin Tran
+--DELETE FROM Customer WHERE Customername = 'Aaron'
+DELETE TOP(18000) FROM Customer
+
+ROLLBACK
+Commit
